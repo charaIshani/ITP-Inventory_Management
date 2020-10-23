a9d17965e6761d2d99ae1934aaae0dd316712084
chara <charaishani123@gmail.com>
2020-10-22 21:13:55 -0700

    commit5

--- src/crud/crud.java
+++ src/crud/crud.java
@@ -77,6 +77,7 @@ public class crud extends javax.swing.JFrame {
         btnPrint = new javax.swing.JButton();
         jTextField1 = new javax.swing.JTextField();
         btnreport = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("CRUD");
@@ -85,7 +86,7 @@ public class crud extends javax.swing.JFrame {
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jTable1.setBackground(new java.awt.Color(153, 255, 153));
-        jTable1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        jTable1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(153, 204, 0), 2, true));
         jTable1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -105,7 +106,7 @@ public class crud extends javax.swing.JFrame {
         });
         jTable1.setColumnSelectionAllowed(true);
         jTable1.setGridColor(new java.awt.Color(0, 0, 0));
-        jTable1.setSelectionBackground(new java.awt.Color(0, 0, 0));
+        jTable1.setSelectionBackground(new java.awt.Color(153, 204, 0));
         jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jTable1MouseClicked(evt);
@@ -205,7 +206,6 @@ public class crud extends javax.swing.JFrame {
         jButton5.setBackground(new java.awt.Color(153, 204, 0));
         jButton5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jButton5.setText("Demo");
-        jButton5.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jButton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton5ActionPerformed(evt);
@@ -219,7 +219,6 @@ public class crud extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(25, 25, 25)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 510, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
@@ -233,10 +232,9 @@ public class crud extends javax.swing.JFrame {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(labvalquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(labvalcategory, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(labvalname, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addGap(386, 386, 386)
-                                        .addComponent(labvalstate, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                    .addComponent(labvalname, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(884, 884, 884)
+                                .addComponent(labvalstate, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                 .addComponent(txtstate, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 397, Short.MAX_VALUE)
                                 .addComponent(txtquantity, javax.swing.GroupLayout.Alignment.LEADING)
@@ -247,7 +245,8 @@ public class crud extends javax.swing.JFrame {
                         .addGap(51, 51, 51)
                         .addComponent(jButton3)
                         .addGap(37, 37, 37)
-                        .addComponent(jButton2)))
+                        .addComponent(jButton2))
+                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 499, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -289,11 +288,11 @@ public class crud extends javax.swing.JFrame {
                         .addGap(77, 77, 77)
                         .addComponent(labvalstate, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
-                .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, 38, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jButton5)
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, 560, 480));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 560, 480));
 
         jPanel2.setBackground(new java.awt.Color(153, 255, 153));
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
@@ -382,6 +381,10 @@ public class crud extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btnreport, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 520, 340, 60));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/crud/photos/000.jpg"))); // NOI18N
+        jLabel7.setText("jLabel7");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 90, 1370, 540));
 
         pack();
         setLocationRelativeTo(null);
@@ -585,14 +588,6 @@ public class crud extends javax.swing.JFrame {
         jTextField1.setForeground(new Color(236,240,241));
     }//GEN-LAST:event_jTextField1FocusLost
 
-    //***********************************Demo Button***************************************************
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        txtname.setText("Shampoo");
-        txtcategory.setText("Bathroom Equipment");
-        txtquantity.setText("150");
-        txtstate.setText("Bellose");
-    }//GEN-LAST:event_jButton5ActionPerformed
-
 //**************************************************Generate Report Button*******************************************************
     private void btnreportActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnreportActionPerformed
         String path ="";
@@ -656,6 +651,14 @@ public class crud extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
 
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        txtname.setText("Shampoo");
+        txtcategory.setText("Bathroom Equipment");
+        txtquantity.setText("150");
+        txtstate.setText("Bellose");
+        
+    }//GEN-LAST:event_jButton5ActionPerformed
+
     private void table_update() {
         int CC;
         try {
@@ -686,9 +689,10 @@ public class crud extends javax.swing.JFrame {
         }
     }
 
+    //****************************************************************Button Round*********************************************************************************
     public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
-        
+        //*******************************************************************************************************************************************************
         
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -710,6 +714,7 @@ public class crud extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
